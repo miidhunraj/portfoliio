3566aeaa786bccdb204b2a2eda92988743f7dfdc
Midhun Raj <mraj43621@gmail.com>
2026-03-09 11:47:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import Portfolio from "./midhun-portfolio";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div>
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
